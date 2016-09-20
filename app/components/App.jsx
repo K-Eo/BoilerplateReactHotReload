d62a7efd987f851a008bf63323de955aa6dfbb1b
Diego Lopez Garcia <aud0l@hotmail.com>
2016-09-20 13:48:08 -0500

    Update package.json

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,7 +4,7 @@ class App extends Component {
   render(): Any {
     return (
       <div>
-        <p>Boilerplate with React, Redux, Jest, JQuery & Sass</p>
+        <p>Boilerplate with React, Redux, Sass, jQuery & Jest</p>
       </div>
     );
   }
