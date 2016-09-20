7fb1be82708587bf8bf1802547d7d99b314a9a85
Diego Lopez Garcia <aud0l@hotmail.com>
2016-09-20 09:30:46 -0500

    Update production script

---
+++ scripts/production.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const express = require('express');
+const util = require('./utils/util');
+const config = require('../webpack.prod.config');
+
+var app = express();
+const PORT = process.env.PORT || process.env.npm_package_config_express || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+util.handleExit();
+util.clear();
+
+console.log('Build for Production\n');
+
+util.clean().then(function() {
+  return util.copyAssets();
+}).then(function() {
+  return new Promise(function(resolve, reject) {
+    const compiler = webpack(config);
+    process.stdout.write('Compiling...');
+    compiler.run(function(error, stats) {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+
+      if (error) {
+        console.log(util.red('Compilation ERROR'));
+        console.log(stats.toString({
+          chunks: false,
+          colors: true
+        }));
+        reject();
+      } else {
+        console.log(util.green('Compilation OK'));
+        resolve(stats.toString({
+          chunks: false,
+          colors: true
+        }));
+      }
+    });
+  });
+}).then(function(stats) {
+  process.stdout.write('Starting server...');
+  app.listen(PORT, function() {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log(util.green('Server OK'));
+    console.log('\n');
+    console.log(stats);
+    console.log('\n');
+    console.log('App is ready');
+  });
+}).catch(function() {});
