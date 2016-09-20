01f787b043e52bbeb2d3967b5baa3fca5e5ffcd2
Diego Lopez Garcia <aud0l@hotmail.com>
2016-09-20 05:35:30 -0500

    Remove GraphQL code and dependencies

--- server.dev.js
+++ server.dev.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./data/schema');
 
 const app = express();
 const PORT = process.env.PORT || process.env.npm_package_config_express || 3000;
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
@@ -15,16 +13,6 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.use('/graphql', graphqlHTTP( () => ({
-  schema
-})));
-
-app.use('/graphqli', graphqlHTTP( () => ({
-  schema,
-  graphiql: true,
-  pretty: true
-})));
-
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   process.stdout.write(`Express server is up on port ${PORT}\n`);
 });
--- server.production.js
+++ server.production.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var schema = require('./data/schema');
 
 var app = express();
 const PORT = process.env.PORT || process.env.npm_package_config_express || 3000;
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
@@ -15,10 +13,6 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.use('/graphql', graphqlHTTP( () => ({
-  schema
-})));
-
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   process.stdout.write(`Express server is up on port ${PORT}\n`);
 });
